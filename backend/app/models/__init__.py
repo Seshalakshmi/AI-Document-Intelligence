e9322f0f7bc9080fccf6f8d940e801d31b1ee074
sesha <sesha@yahoo.com>
2026-06-26 10:49:49 +0200

    Implements document model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 # helps to reads all of them together
 from app.models.document import Document
-from app.models.document_chunks import DocumentChunk
+from app.models.document_chunk import DocumentChunk
 
 # work together on document_chunk
