b6bda7c2122f6506d037b0233fb1ebdff9b1b718
sesha <sesha@yahoo.com>
2026-07-01 10:09:46 +0200

    created user table

---
+++ backend/app/models/user.py
@@ -0,0 +1,20 @@
+#create users class
+# id, email, fullname, is_active -> True, false, is_admin -> true, false
+# created_at, 
+from app.db.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, Boolean
+from datetime import datetime, timezone
+
+class Users(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    fullname: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255))
+    role: Mapped[str] = mapped_column(String(255))
+    
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
