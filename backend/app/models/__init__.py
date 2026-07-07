e762d242ec5289c47f82667e65e72cdb22962df6
sesha <sesha@yahoo.com>
2026-07-07 12:04:06 +0200

    modified init.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 # helps to reads all of them together
 from app.models.document import Document
 from app.models.document_chunk import DocumentChunk
-
-# work together on document_chunk
+from app.models.user import Users
+from extracted_invoice_data import ExtractedInvoiceData
