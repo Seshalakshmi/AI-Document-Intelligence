bed2f45cf87b470be7a2d7386f5b569502ec8f65
sesha <sesha@yahoo.com>
2026-07-09 11:39:38 +0200

    modified extracted data in models

--- backend/app/models/extracted_data.py
+++ backend/app/models/extracted_data.py
@@ -1,4 +1,4 @@
-from app.db.database import Base
+from app.db.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, ForeignKey, Text, Float, DateTime
 from datetime import datetime, timezone
