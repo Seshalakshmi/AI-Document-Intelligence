edbf241767dddbae2b5471ae22216add27243927
sesha <sesha@yahoo.com>
2026-07-09 11:36:55 +0200

    initialized routers for search

---
+++ backend/app/api/routes/search.py
@@ -0,0 +1,36 @@
+# / search ({message})
+# / semantic_search ({message})
+# / search_similar_document_id ({message_id})
+# / search_duplicated_document ({message_id})
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/search", tags=["SEARCH"])
+
+@router.get("/")
+def search():
+    return {
+        "status": "ok",
+        "message": "Implemention search"
+    }
+
+
+@router.get("/")
+def semantic_search():
+    return {
+        "status": "ok",
+        "message": "semantic search"
+    }
+
+@router.get("/{document_id}")
+def search_similar_document_id(document_id: int):
+    return {
+        "status": "ok",
+        "message": "search similar documents by document_id"
+    }
+
+@router.get("/document_id")
+def search_duplicated_document(document_id: int):
+    return {
+        "status": "ok",
+        "message": "search duplicated document"
+    }
