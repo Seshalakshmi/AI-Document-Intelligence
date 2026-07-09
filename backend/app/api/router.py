01322292fb88d4c6c9e06d3f73f48ba8ef80868d
sesha <sesha@yahoo.com>
2026-07-09 11:33:14 +0200

    modified router.py

---
+++ backend/app/api/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.api.routes import user, documents, search, health
+
+api_router = APIRouter()
+api_router.include_router(user.router)
+# api_router.include_router(documents.router)
+api_router.include_router(search.router)
+api_router.include_router(health.router)
