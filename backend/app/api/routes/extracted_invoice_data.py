5988e49d7ba576f25e9feb86e17793a7a11a9931
sesha <sesha@yahoo.com>
2026-07-10 10:14:13 +0200

    modified routes to extracted invoice data

--- backend/app/api/routes/extracted_invoice_data.py
+++ backend/app/api/routes/extracted_invoice_data.py
@@ -2,57 +2,57 @@ from fastapi import APIRouter, Depends, Body, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from app.db.database import get_db
-from app.models.document_chunk import DocumentChunk
+from app.models.extracted_invoice_data import ExtractedInvoiceData
 
-router = APIRouter(prefix="/document_chunks", tags=["DOCUMENT_CHUNKS"])
+router = APIRouter(prefix="/extracted_invoice_data", tags=["EXTRACTED_INVOICE_DATA"])
 
 
 @router.get("/")
 def get_document(db: Session = Depends(get_db)):
-    document_chunks = db.query(DocumentChunk).all()
+    invoice_data = db.query(ExtractedInvoiceData).all()
 
-    if not document_chunks:
-        raise HTTPException(status_code=404,detail="No users found.")
+    if not invoice_data:
+        raise HTTPException(status_code=404,detail="No invoice data found.")
     
-    return document_chunks
+    return invoice_data
 
 
 @router.post("/")
-def upload_document(document_chunk: dict = Body(...), db: Session = Depends(get_db)):
+def upload_document(invoice_data: dict = Body(...), db: Session = Depends(get_db)):
     try:
-        new_document_chunk = DocumentChunk(
-            document_id=document_chunk["document_id"],
-            supplier_name=document_chunk["supplier_name"],
-            invoice_number=document_chunk["invoice_number"],
-            invoice_date=document_chunk["invoice_date"],
-            currency=document_chunk["currency"],
-            subtotal=document_chunk["subtotal"],
-            tax_amount=document_chunk["tax_amount"],
-            total_amount=document_chunk["total_amount"],
-            payment_terms=document_chunk["payment_terms"],
-            confidence_score=document_chunk["confidence_score"],
-            raw_extraction_json=document_chunk["raw_extraction_json"],
-            is_reviewed=document_chunk["is_reviewed"]
+        new_invoice_data = ExtractedInvoiceData(
+            document_id=invoice_data["document_id"],
+            supplier_name=invoice_data["supplier_name"],
+            invoice_number=invoice_data["invoice_number"],
+            invoice_date=invoice_data["invoice_date"],
+            currency=invoice_data["currency"],
+            subtotal=invoice_data["subtotal"],
+            tax_amount=invoice_data["tax_amount"],
+            total_amount=invoice_data["total_amount"],
+            payment_terms=invoice_data["payment_terms"],
+            confidence_score=invoice_data["confidence_score"],
+            raw_extraction_json=invoice_data["raw_extraction_json"],
+            is_reviewed=invoice_data["is_reviewed"]
         )
 
-        db.add(new_document_chunk)
+        db.add(new_invoice_data)
         db.commit()
-        db.refresh(new_document_chunk)
+        db.refresh(new_invoice_data)
 
         return {
-            "message": "User successfully created",
-            "user": new_document_chunk
+            "message": "Invoice data successfully created",
+            "user": new_invoice_data
         }
     except SQLAlchemyError as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/{document_chunk_id}")
-def get_user_by_id(document_chunk_id:int, db: Session = Depends(get_db)):
-    document_chunk = db.query(DocumentChunk).filter(DocumentChunk.id == document_chunk_id).first()
+@router.get("/{document_id}")
+def get_user_by_id(document_id:int, db: Session = Depends(get_db)):
+    invoice_data = db.query(ExtractedInvoiceData).filter(ExtractedInvoiceData.document_id == document_id).first()
 
-    if not document_chunk:
-        raise HTTPException(status_code=404,detail="No users found.")
+    if not invoice_data:
+        raise HTTPException(status_code=404,detail="No invoices found.")
     
-    return document_chunk
+    return invoice_data
