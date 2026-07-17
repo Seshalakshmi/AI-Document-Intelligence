50df160c7cd8b84c74bb1754af0aff5be60523eb
sesha <sesha@yahoo.com>
2026-07-17 11:08:08 +0200

    upgraded config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,20 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     app_name: str = "AI Document Intelligence"
     app_env: str = "development"
-    database_url: str 
+
+    database_url: str
     upload_dir: str = "uploads"
 
-    class Config:
-        env_file: str = ".env"
-    
+    embedding_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    embedding_dimension: int = 384
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+
 settings = Settings()
